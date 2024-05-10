629990854a120fd35831d199a8dc650abb8afd31
xsaven <gms.sw.xsaven@gmail.com>
2024-05-10 17:38:42 +0300

    public/q.php

--- public/q.php
+++ public/q.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 
-$indexes = file_get_contents('https://raw.githubusercontent.com/Xsaven/bfg-admin-documentation/master/Writerside/index.json');
+$indexes = file_get_contents('https://raw.githubusercontent.com/bfg-s/admin-documentation/master/Writerside/index.json');
 $indexes = json_decode($indexes, true);
 
 $resultTitles = [];
